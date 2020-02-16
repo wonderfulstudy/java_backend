1f13880b6b0bf747d440d4a0500e3e8d06cf886f
jiang_yi_feng <5470678+jiang_yi_feng@user.noreply.gitee.com>
2020-02-16 14:00:28 +0800

    添加Java基础练习项目

---
+++ Java核心基础/练习项目/demo002.java
@@ -0,0 +1,13 @@
+/***
+ * 
+ * 使用JDK内置方法实现数组降序排序
+ */
+
+public class demo002{
+    public static void main(String[] args){
+        int[] ns = {12,45,56,78,69,25,34,63,15,2,6,8};
+        System.out.println(ns);
+        //排序前
+        Arrays.sort(ns,)
+    }
+}
